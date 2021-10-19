4f8256a8559c44de4884542b9271f6d0250f2d11
usausa <machi.pon@gmail.com>
2021-10-19 15:53:41 +0900

    Fix inspection

--- Template.Web/Areas/Api/Models/TerminalTimeResponse.cs
+++ Template.Web/Areas/Api/Models/TerminalTimeResponse.cs
@@ -2,8 +2,6 @@ namespace Template.Web.Areas.Api.Models
 {
     using System;
 
-    using Template.Models.Entity;
-
     public class TerminalTimeResponse
     {
         public DateTime DateTime { get; set; }
