b25c031a2600d0da0dadaed6cae273b7f15e4abf
usausa <machi.pon@gmail.com>
2021-10-19 16:16:41 +0900

    Update setting

---
+++ publish-linux-x64-single.bat
@@ -0,0 +1,1 @@
+dotnet publish Template.Web\Template.Web.csproj -o Publish\linux-x64 -c Release -r linux-x64 /p:PublishSingleFile=true --self-contained false
---
+++ publish-win10-x64-single.bat
@@ -0,0 +1,1 @@
+dotnet publish Template.Web\Template.Web.csproj -o Publish\win10-x64 -c Release -r win10-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true --self-contained false
