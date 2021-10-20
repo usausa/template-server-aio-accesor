36821d32540bd85b443a5d356b04e18d80fe24fa
usausa <machi.pon@gmail.com>
2021-10-20 15:59:54 +0900

    Add test project

---
+++ Template.Tests/Assembly.cs
@@ -0,0 +1,13 @@
+using System;
+
+[assembly: CLSCompliant(false)]
+
+// ReSharper disable CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    // For compatibility
+    public class IsExternalInit
+    {
+    }
+}
+// ReSharper restore CheckNamespace
---
+++ Template.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Template
+{
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // TODO
+        }
+    }
+}
