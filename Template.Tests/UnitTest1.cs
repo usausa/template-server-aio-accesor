06075022cd45b6bf720e0f920bff3decc2cca43a
usausa <machi.pon@gmail.com>
2021-10-20 18:05:51 +0900

    Update test

--- Template.Tests/UnitTest1.cs
+++ Template.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 namespace Template
 {
+    using Template.Models.Entity;
+
     using Xunit;
 
     public class UnitTest1
@@ -8,6 +10,8 @@ namespace Template
         public void Test1()
         {
             // TODO
+            var entity = new AccountEntity { Id = "00000000", Name = "管理者", IsAdmin = true, PasswordHash = string.Empty };
+            Assert.Equal("00000000", entity.Id);
         }
     }
 }
