e5a81503d10a546459b95bec2b0adb3efb79627f
usausa <machi.pon@gmail.com>
2021-11-20 13:07:09 +0900

    Update

--- Template.Tests/Assembly.cs
+++ Template.Tests/Assembly.cs
@@ -1,11 +1,1 @@
 [assembly: CLSCompliant(false)]
-
-// ReSharper disable CheckNamespace
-namespace System.Runtime.CompilerServices
-{
-    // For compatibility
-    public class IsExternalInit
-    {
-    }
-}
-// ReSharper restore CheckNamespace
