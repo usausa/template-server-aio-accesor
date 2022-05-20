67d07bd04f10900b2d71ec85a54f899528273504
usausa <machi.pon@gmail.com>
2022-05-20 19:34:58 +0900

    Update

--- Template.Web/Areas/Api/BaseApiController.cs
+++ Template.Web/Areas/Api/BaseApiController.cs
@@ -3,6 +3,7 @@ namespace Template.Web.Areas.Api;
 [Area("api")]
 [Route("[area]/[controller]/[action]")]
 [ApiController]
+[ApiExceptionFilter]
 [TokenFilter]
 public class BaseApiController : ControllerBase
 {
