7f629aaef6f44cf8135a326a5780b3c77e27e1c1
usausa <machi.pon@gmail.com>
2024-02-06 13:49:38 +0900

    Update setting

--- Template.Web/Areas/Api/Controllers/Log.cs
+++ Template.Web/Areas/Api/Controllers/Log.cs
@@ -1,7 +1,6 @@
 namespace Template.Web.Areas.Api.Controllers;
 
-#pragma warning disable SYSLIB1006
-public static partial class Log
+internal static partial class Log
 {
     [LoggerMessage(Level = LogLevel.Information, Message = "Get. path=[{path}]")]
     public static partial void InfoStorageGet(this ILogger logger, string? path);
@@ -15,4 +14,3 @@ public static partial class Log
     [LoggerMessage(Level = LogLevel.Warning, Message = "No found. path=[{path}]")]
     public static partial void WarnStorageNotFound(this ILogger logger, string? path);
 }
-#pragma warning restore SYSLIB1006
