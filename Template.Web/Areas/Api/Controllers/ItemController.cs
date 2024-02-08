5cc119a3cec51fc5450fb14d02b1534571ae3acf
usausa <machi.pon@gmail.com>
2024-02-08 19:12:07 +0900

    Update library

--- Template.Web/Areas/Api/Controllers/ItemController.cs
+++ Template.Web/Areas/Api/Controllers/ItemController.cs
@@ -28,7 +28,7 @@ public sealed class ItemController : BaseApiController
     [HttpPost]
     public async ValueTask<IActionResult> Update([FromBody] ItemUpdateRequest request)
     {
-        await ItemService.UpdateItemList(request.Entries.Select(x => Mapper.Map<ItemUpdateRequestEntry, ItemEntity>(x)!));
+        await ItemService.UpdateItemList(request.Entries.Select(Mapper.Map<ItemUpdateRequestEntry, ItemEntity>));
 
         return Ok();
     }
