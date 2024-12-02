2b7ce5adeb527696df6fa3bacd8a004c4677aeb0
usausa <machi.pon@gmail.com>
2024-12-02 11:29:40 +0900

    Update setting

--- Template.Tests/GlobalSuppressions.cs
+++ Template.Tests/GlobalSuppressions.cs
@@ -1,2 +1,3 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Ignore")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "Ignore")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2234:Pass System.Uri objects instead of strings", Justification = "Ignore")]
